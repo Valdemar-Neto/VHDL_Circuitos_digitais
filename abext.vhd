cabdc9746e732b3133ebf3e6861f23097ddf0a19
helio04 <136762968+helio04@users.noreply.github.com>
2024-10-03 18:25:24 -0300

    Add files via upload

--- abext.vhd
+++ abext.vhd
@@ -1,7 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
------ entidade abext: entradas a e b de 8 bits, k de 4 bits; saidas: ia e ib de 8 bits, cin de 1 bit;
------ componente principal do extensor AL
+
 entity abext is
 port(a,b : in std_logic_vector(7 downto 0); 
 k : in std_logic_vector(3 downto 0);
@@ -10,22 +9,32 @@ ia, ib : out std_logic_vector(7 downto 0));
 end abext;
 
 architecture extensor of abext is
-signal tempa : std_logic_vector(7 downto 0);
-signal tempb : std_logic_vector(7 downto 0);
+signal nota : std_logic_vector(7 downto 0);
+signal notb : std_logic_vector(7 downto 0);
 signal saidaxor : std_logic_vector(7 downto 0);
 component xor8bits is
 	port(a,b : in std_logic_vector(7 downto 0); z : out std_logic_vector(7 downto 0));
 end component xor8bits;
 begin
+nota <= not a;
+notb <= not b;
 xorgate: xor8bits port map(a,b, saidaxor);
 with k select
-	ia <= saidaxor when "1110", 
+	ia <= a when "0001",
+	a when "0010",
+	a when "0100",
+	(a and b)when "1100",
+	(a or b) when "1101",
+	saidaxor when "1110",
+	nota when "1111",
 a when others;
 
 with k select
-	ib <= "00000000" when "1110", 
-b when others;
+	ib <= b when "0001",
+	notb when "0010", 
+	"00000000" when others;
 with k select
 	cin <= '1' when "0010",
+	'1' when "0100",
 	'0' when others;
 end architecture; 
