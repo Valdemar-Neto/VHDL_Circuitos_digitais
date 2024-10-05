ff799ef4d2f335b7e51364626270d7568ea87c63
Rutileno Gabriel <92885893+BigLeno@users.noreply.github.com>
2024-10-05 17:00:35 -0300

    Modificando as entradas e saídas

--- Multiplicador/VHDL/fulladder.vhd
+++ Multiplicador/VHDL/fulladder.vhd
@@ -1,33 +1,33 @@
 
 -- Autor: Rutileno Gabriel Camilo da Silva
 
--- Definindo o fulladder
--- A partir de algumas portas lógicas, é possível implementar um fulladder
+-- Definindo o fulladder_mult
+-- A partir de algumas portas lógicas, é possível implementar um fulladder_mult
 
 
 -- Importando as bibliotecas permitidas
 library ieee;
 use ieee.std_logic_1164.all;
 
--- Definindo a entidade fulladder
-entity fulladder is
+-- Definindo a entidade fulladder_mult
+entity fulladder_mult is
     port (
 
-        -- Definindo as entradas e saídas do fulladder
-        A_input, B_input, Carry_in : in std_logic;
-        S_output, Carry_out : out std_logic
+        -- Definindo as entradas e saídas do fulladder_mult
+        A_fa_mult_input, B_fa_mult_input, Carry_fa_mult_in : in std_logic;
+        S_fa_mult_output, Carry_fa_mult_out : out std_logic
     );
 
--- Fim da entidade fulladder
-end entity fulladder;
+-- Fim da entidade fulladder_mult
+end entity fulladder_mult;
 
--- Definindo a arquitetura do fulladder
-architecture ckt of fulladder is
+-- Definindo a arquitetura do fulladder_mult
+architecture ckt of fulladder_mult is
 
     begin
         -- Atribuindo as operações lógicas para os sinais de saída
-        S_output <= A_input xor B_input xor Carry_in;
-        Carry_out <= (A_input and B_input) or (Carry_in and (A_input xor B_input));
+        S_fa_mult_output <= A_fa_mult_input xor B_fa_mult_input xor Carry_fa_mult_in;
+        Carry_fa_mult_out <= (A_fa_mult_input and B_fa_mult_input) or (Carry_fa_mult_in and (A_fa_mult_input xor B_fa_mult_input));
 
 -- Fim da arquitetura ckt
 end architecture ckt;
