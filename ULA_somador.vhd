46959c1c6ab8b81695d02c62bf36ed56dea490bb
Valdemar Neto <108936921+Valdemar-Neto@users.noreply.github.com>
2024-10-06 11:58:06 -0300

    Update ULA_somador.vhd

--- ULA_somador.vhd
+++ ULA_somador.vhd
@@ -4,25 +4,40 @@ use ieee.std_logic_1164.all;
 entity ULA_somador is
 	port(
 		A_somador, B_somador: in std_logic_vector(7 downto 0);
-		cin : in std_logic;
-		soma : out std_logic_vector (7 downto 0);
-		carry_out : out std_logic
+		Cin_somador : in std_logic;
+		S_somador : out std_logic_vector (7 downto 0);
+		Cout_somador : out std_logic
 	);
 end ULA_somador;
 
 architecture behavior of ULA_somador is
-	signal carry: std_logic_vector(8 downto 0);
-begin 
-	---inicializando o carry com valor de cout
-	carry(0) <= cin;
-	
-	process(A_somador, B_somador)
-	begin
-		for i in 0 to 7 loop
-			carry(i+1) <=( A_somador(i) and B_somador(i)) or (carry(i) and (A_somador(i) xor B_somador(i)));
-			soma(i) <= 	A_somador(i) xor B_somador(i) xor carry(i); ----SOMA
-		end loop;
-		
-		carry_out <= carry(8);
-	end process;
+ signal carry : std_logic_vector(7 downto 0); -- Carries intermediários
+begin
+    -- Soma bit a bit e geração de carry
+    S_somador(0) <= A_somador(0) xor B_somador(0) xor Cin_somador;  -- Soma do primeiro bit com carry in
+    carry(0) <= (A_somador(0) and B_somador(0)) or (Cin_somador and (A_somador(0) xor B_somador(0)));
+
+    S_somador(1) <= A_somador(1) xor B_somador(1) xor carry(0);
+    carry(1) <= (A_somador(1) and B_somador(1)) or (carry(0) and (A_somador(1) xor B_somador(1)));
+
+    S_somador(2) <= A_somador(2) xor B_somador(2) xor carry(1);
+    carry(2) <= (A_somador(2) and B_somador(2)) or (carry(1) and (A_somador(2) xor B_somador(2)));
+
+    S_somador(3) <= A_somador(3) xor B_somador(3) xor carry(2);
+    carry(3) <= (A_somador(3) and B_somador(3)) or (carry(2) and (A_somador(3) xor B_somador(3)));
+
+    S_somador(4) <= A_somador(4) xor B_somador(4) xor carry(3);
+    carry(4) <= (A_somador(4) and B_somador(4)) or (carry(3) and (A_somador(4) xor B_somador(4)));
+
+    S_somador(5) <= A_somador(5) xor B_somador(5) xor carry(4);
+    carry(5) <= (A_somador(5) and B_somador(5)) or (carry(4) and (A_somador(5) xor B_somador(5)));
+
+    S_somador(6) <= A_somador(6) xor B_somador(6) xor carry(5);
+    carry(6) <= (A_somador(6) and B_somador(6)) or (carry(5) and (A_somador(6) xor B_somador(6)));
+
+    S_somador(7) <= A_somador(7) xor B_somador(7) xor carry(6);
+    carry(7) <= (A_somador(7) and B_somador(7)) or (carry(6) and (A_somador(7) xor B_somador(7)));
+
+    -- Carry de saída
+    Cout_somador <= carry(7);
 end behavior;
